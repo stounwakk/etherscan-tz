147a13ff2c30f281799ff7d1a7704956036d2e0e
stounwakk <stounwakk@gmail.com>
2021-11-08 16:26:25 +0300

    fixes

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -84,7 +84,7 @@ class statsController {
                })
 
            const maxAddressBalance = Object.keys(addressBalance).reduce((acc, curr) =>
-               acc.balance ? (addressBalance[curr].balance > acc.balance ? curr : acc) : curr, {});
+               acc.balance ? (addressBalance[curr].balance > acc.balance ? addressBalance[curr] : acc) : addressBalance[curr], {});
            res.json(maxAddressBalance)
        } catch (e) {
            console.log(e)
